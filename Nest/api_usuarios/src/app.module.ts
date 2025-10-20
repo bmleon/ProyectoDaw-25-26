8e2a38dad08a9d5b0f9a77e03a6cfe47ec1f9ede
Belén María León <belenleon10.98@gmail.com>
2025-10-20 19:42:04 +0200

    Enlace con la base de datos

--- Nest/api_usuarios/src/app.module.ts
+++ Nest/api_usuarios/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsuariosModule],
+  imports: [UsuariosModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '38.242.240.106',
+      port: 5432,
+      database: 'bdusuarios',
+      username: 'admin',
+      password: 'admin',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
