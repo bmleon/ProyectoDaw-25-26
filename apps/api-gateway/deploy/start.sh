3309441d9de7efa621d7a9750b10da30e983ade9
root <prueba@gmail.com>
2026-01-20 14:12:19 +0100

    Deploy: github keys

--- apps/api-gateway/deploy/start.sh
+++ apps/api-gateway/deploy/start.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -e
 
+INFORME=/root/logs/informe.log
+config_git(){
+    git clone --filter=blob:none --no-checkout $REPO_GIT ukiyo-backend
+    cd ukiyo-backend
+
+    # con sparse-checkout init --cone --> copiamos los archivos directos del monorepositorio
+    # package.json, pnpm-lock.yaml, pnpm-workspace.yaml, tsconfig.json, .npmrc, .prettierrc, .eslintrc.json, .gitignore, etc
+    git sparse-checkout init --cone
+
+    # Trae solo los subdirectorios que indiques (apps/${MICROSERVICIO} y libs)
+    git sparse-checkout set apps/${MICROSERVICIO} libs
+    git checkout main
+
+    git pull origin main
+
+}
+
 main(){
 
     npm install -g npm@11.7.0
