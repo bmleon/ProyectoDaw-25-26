7e408157954c654a57ae38b3884d2ce3c14f6bf5
root <prueba@gmail.com>
2026-02-04 16:50:08 +0100

    Despliegue kubernetes: ms-usuarios(Seed)

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsuariosModule } from './modulos/usuarios/usuarios.module';
 import { ClientesModule } from './modulos/clientes/clientes.module';
-import { SeedModule } from './modulos/seed/seed.module';
 import { ProductosModule } from './modulos/productos/productos.module';
 import { AuthModule } from './modulos/auth/auth.module';
 
@@ -14,7 +13,6 @@ import { AuthModule } from './modulos/auth/auth.module';
     UsuariosModule,
     ClientesModule,
     ProductosModule,
-    SeedModule,
     AuthModule,
   ],
   controllers: [],
