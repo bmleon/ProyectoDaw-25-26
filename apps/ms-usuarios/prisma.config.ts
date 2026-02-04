e0b0816f361af2ff8901c085451187dbb40a3453
root <prueba@gmail.com>
2026-02-04 20:07:18 +0100

    Despliegue kubernetes: ms-usuarios(Seed)

--- apps/ms-usuarios/prisma.config.ts
+++ apps/ms-usuarios/prisma.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
-    seed: 'npx ts-node prisma/seed.ts',
+    seed: 'ts-node prisma/seed.ts',
     path: 'prisma/migrations',
   },
   datasource: {
